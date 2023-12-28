0593889b27c0e066cfb6e3bbc91586470bb0de71
housekjh1 <141015592+housekjh1@users.noreply.github.com>
2023-12-28 18:05:46 +0900

    로그인 api 테스트 중

--- src/component/Login.js
+++ src/component/Login.js
@@ -38,7 +38,8 @@ const Login = () => {
                 <div className="bg-blue-50 border-0 rounded-lg w-[300px] h-[300px] overflow-hidden shadow-[0px_0px_40px_-10px_rgba(0,0,0,0.3)]">
                     <div className="flex flex-col justify-center items-center">
                         <div className="bg-[#0b3565] w-full h-[85px]"><p className="text-3xl font-bold text-white text-center pt-[20px]">Login</p></div>
-                        <Link to="http://10.125.121.217:8080/auth/naver">
+                        {/* <Link to="http://10.125.121.217:8080/auth/naver"> */}
+                        <Link to="http://localhost:8080/oauth2/naver">
                             <div className="mt-[80px] flex justify-center items-center gap-1">
                                 <img className="w-[50px] h-[50px] rounded" src={Naver} />
                                 <p className="flex justify-center items-center bg-gradient-to-b from-[#03E965] to-[#03B964] rounded w-[190px] h-[50px] text-white text-lg font-bold">네이버로 로그인하기</p>
